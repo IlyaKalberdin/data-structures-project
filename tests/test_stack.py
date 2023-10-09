f0368d0f1d733d86a3a85a6f7b2afe3906085b18
Ilya Kalberdin <ilyakalberdin@gmail.com>
2023-10-09 06:34:56 +0300

    Написаны тесты для метода pop и добавлена фикстура

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,6 +1,10 @@
 import pytest
-
 from src.stack import Node, Stack
+
+
+@pytest.fixture
+def test_stack():
+    return Stack()
 
 
 def test_Node():
@@ -9,22 +13,30 @@ def test_Node():
     test_node2 = Node(20, test_node1)
 
     assert test_node1.data == 10
-    assert test_node1.next_node == None
+    assert test_node1.next_node is None
     assert test_node2.data == 20
     assert test_node2.next_node == test_node1
 
 
-def test_Stack():
-    """Функция для тестирования класса Stack"""
-    test_stack = Stack()
-
+def test_Stack_push(test_stack):
+    """Функция для тестирования метода push класса Stack"""
     test_stack.push(1)
     test_stack.push(2)
 
     assert test_stack.top.data == 2
     assert test_stack.top.next_node.data == 1
-    assert test_stack.top.next_node.next_node == None
+    assert test_stack.top.next_node.next_node is None
 
     # Перехват ожидаемой ошибки
     with pytest.raises(AttributeError):
         test_stack.top.next_node.next_node.data
+
+
+def test_Stack_pop(test_stack):
+    """Функция для тестирования метода pop класса Stack"""
+    test_stack.push(5)
+    test_stack.push(3)
+    data = test_stack.pop()
+
+    assert data == 3
+    assert test_stack.top.data == 5
