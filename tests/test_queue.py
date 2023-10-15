667a9d3c425e796288dd88ed78c20f23296fce71
Ilya Kalberdin <ilyakalberdin@gmail.com>
2023-10-15 11:44:57 +0300

    Добавлены тесты для классов Node и Queue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,39 @@
-"""Здесь надо написать тесты с использованием unittest для модуля queue."""
+import pytest
+from src.queue import Node, Queue
+
+
+@pytest.fixture
+def test_queue():
+    return Queue()
+
+
+def test_Node():
+    """Функция для тестирования класса Node"""
+    test_node1 = Node(1, None)
+    test_node2 = Node(2, None)
+    test_node1.next_node = test_node2
+
+    assert test_node1.data == 1
+    assert test_node1.next_node == test_node2
+    assert test_node2.data == 2
+    assert test_node2.next_node is None
+
+
+def test_Queue_enqueue(test_queue):
+    """Функция для тестирования метода enqueue класса Queue"""
+    test_queue.enqueue(1)
+    test_queue.enqueue(2)
+    test_queue.enqueue(3)
+    test_queue.enqueue(4)
+
+    assert str(test_queue) == "1\n2\n3\n4"
+
+    assert test_queue.head.data == 1
+    assert test_queue.tail.data == 4
+    assert test_queue.tail.next_node is None
+    assert test_queue.head.next_node.next_node.data == 3
+
+
+def test_Queue_dequeue(test_queue):
+    """Функция для тестирования метода dequeue класса Queue"""
+    pass
